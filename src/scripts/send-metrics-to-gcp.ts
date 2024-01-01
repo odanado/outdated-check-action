e35e910dc04470e8af1516c585a5e90121793642
odan <odan3240@gmail.com>
2024-01-02 05:55:14 +0900

    fix

--- src/scripts/send-metrics-to-gcp.ts
+++ src/scripts/send-metrics-to-gcp.ts
@@ -84,7 +84,7 @@ const resource = {
 const keys = Object.keys(result) as (keyof Result)[];
 
 const timeSeries = keys.map((key): protos.google.monitoring.v3.ITimeSeries => {
-  const valueKey = Number.isInteger(result[key]) ? "int64Value" : "doubleValue";
+  const valueKey = key.includes("Count") ? "int64Value" : "doubleValue";
   const point = {
     interval,
     value: {
